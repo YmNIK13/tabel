8c187309226c6ab84d741c647a8668d044406a0c
YmNIK13 <valery.stavitsky@gmail.com>
2019-03-10 19:46:49 +0200

    add user hook "hook_report_cart_block_set_date"

--- modules/custom/report_cart_block/src/Plugin/Block/ReportBlock.php
+++ modules/custom/report_cart_block/src/Plugin/Block/ReportBlock.php
@@ -18,9 +18,34 @@ namespace Drupal\report_cart_block\Plugin\Block;
  */
 class ReportBlock extends \Drupal\Core\Block\BlockBase {
 
+  private $_date;
 
-  static function getDateReport() {
-    // получаем ID ноды
+  private $_cur_month;
+
+  private $_cur_year;
+
+  private function init() {
+    // hook_report_cart_block_set_date().
+    $cur_date = \Drupal::moduleHandler()
+      ->invokeAll('report_cart_block_set_date');
+
+
+    if (empty($cur_date)) {
+      $cur_date = time();
+    }
+
+    $this->_cur_month = date('n', $cur_date);
+    $this->_cur_year = date('Y', $cur_date);
+
+    // указываем первый день месяца
+    $date_first_day_mouth = new \DateTime();
+    $date_first_day_mouth->setDate($this->_cur_year, $this->_cur_month, 1);
+
+    $this->_date = $date_first_day_mouth->getTimestamp();
+  }
+
+  private function getDatesThisMonth() {
+    // получаем ID ноды из пути
     $nid = \Drupal::routeMatch()->getRawParameter('node');
 
     if (empty($nid)) {
@@ -33,29 +58,36 @@ class ReportBlock extends \Drupal\Core\Block\BlockBase {
     }
 
 
-    $dates_works = $node->field_worked->getValue();
-
-
-    $result_date = [];
-
-    foreach ($dates_works as $work){
-      $result_date[] = date_parse_from_format('Y-m-d',$work['value']);
+    $work_days = [];
+    foreach ($node->field_worked->getValue() as $work) {
+      $work_date_array = date_parse_from_format('Y-m-d', $work['value']);
+      // выбираем даты текущего месяца
+      if (!empty($work_date_array)
+        && ($work_date_array['year'] == $this->_cur_year)
+        && ($work_date_array['month'] == $this->_cur_month)) {
+        $work_days[] = $work_date_array['day'];
+      }
     }
 
+    return $work_days;
+  }
 
-    return [
-      'dates' => $result_date,
-    ];
+
+  private function getNumberWeekDay() {
+    return (date('N', $this->_date));
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $this->init();
+
     return [
       '#theme' => 'report_cart_block',
-      '#dates' =>  ReportBlock::getDateReport(),
-      '#week_day' => 5,
+      '#work_days' => $this->getDatesThisMonth(),
+      '#week_day' => $this->getNumberWeekDay(),
+      '#this_day' => $this->_date,
     ];
   }
 }
